5175ce723662d7b1df4e7b496839ea8a5736ffac
Vaansh <90090271+Vaansh-GitHub@users.noreply.github.com>
2025-12-10 02:04:06 +0530

    Removed Bugs from skeletonLogic

--- src/skeletonLogic.js
+++ src/skeletonLogic.js
@@ -15,7 +15,8 @@ export function makeSkeleton(k, initialPos) {
         {
             speed: 20,
             pursuitSpeed: 40,
-            range: 90,
+            range: 100,
+
             setBehaviour() {
                 const player = k.get("player", { recursive: true })[0]//As the player is the part of map object hence it searches for it recursively
                 this.onStateEnter("patrol-right", async () => {
@@ -69,22 +70,23 @@ export function makeSkeleton(k, initialPos) {
                         return;
                     }
                     this.flipX = player.pos.x <= this.pos.x;
-                    this.moveTo(k.vec2(player.pos.x + 20, player.pos.y), this.pursuitSpeed)//Function offered by kaboom to body containing elements it takes position and then a speed 
+                    this.moveTo(k.vec2(player.pos.x + 55, player.pos.y), this.pursuitSpeed)//Function offered by kaboom to body containing elements it takes position and then a speed 
                 })
+
             },
             setEvents() {
                 const player = k.get("player", { recursive: true })[0];
                 // Attack repeatedly while player remains colliding/overlapping
-                this._attackCooldown = 0;
+                this._attackCooldown = 2;
                 this._isPerformingAttack = false;
                 //onUpdate() function runs on a component every 60 frames
                 this.onUpdate(() => {
-                    if (!player || !player.exists()) return;
+                    if (!player || !player.exists() || player.hp() <= 0) return;
                     // decrease cooldown every second
                     if (this._attackCooldown > 0) this._attackCooldown -= k.dt();
 
                     // if currently colliding with player and cooldown expired, perform an attack
-                    if (this.isColliding(player) && this._attackCooldown <= 0) {
+                    if (this.pos.dist(player.pos) < 60 && this._attackCooldown <= 0) {
                         this._attackCooldown = 0.8; // seconds between attacks
                         // perform attack
                         this._isPerformingAttack = true;
@@ -93,8 +95,8 @@ export function makeSkeleton(k, initialPos) {
                         const offX = (player.pos.x <= this.pos.x ? -60 : 0);
                         const skeletonHitBox = this.add([
                             k.pos(this.flipX ? -25 : 0, 10),
-                            k.area({ shape: new k.Rect(k.vec2(offX, -30), 60, 45) }),
-                            "skeleton-hitbox",
+                            k.area({ shape: new k.Rect(k.vec2(offX, -30), 70, 45) }),
+                            "creature-hitbox",
                         ]);
                         // destroy hitbox after animation window
                         k.wait(0.7, () => {
